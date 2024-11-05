3d905b737c2df7ce9224e760e8b5ff0b2bccb140
matercomus <matek.kedzia@gmail.com>
2024-11-05 11:17:11 +0100

    dtest

--- demos/nao/demo_nao_motion.py
+++ demos/nao/demo_nao_motion.py
@@ -5,6 +5,7 @@ from sic_framework.devices.common_naoqi.naoqi_motion import NaoPostureRequest
 
 nao = Nao(ip="192.168.0.0")
 
+# Stand up
 reply = nao.motion.request(NaoPostureRequest("Stand", 0.5))
 time.sleep(1)
 
